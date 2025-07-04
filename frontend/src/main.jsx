4fc641b9dfcfb085c4b4a16b452d6ab31220df21
Akash Verma <akashvermastp7@gmail.com>
2025-07-04 16:44:50 +0530

    update files

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -11,7 +11,7 @@ import { Helmet } from "react-helmet";
   <meta name="description" content="Track your income, expenses, and savings easily with finSecure – a secure personal finance tracker web app." />
   <meta name="keywords" content="finance tracker, personal finance, budget app, expense manager, finSecure, React finance app" />
   <meta name="author" content="Akash Verma" />
-  <meta name="google-site-verification" content="googleddcb8994e3c33749" />
+  <meta name="google-site-verification" content="KZDj_6Gseo2H8yzO6sLBYzUkZ4nSeyc62MJvAbz7JSc" />
 </Helmet>
 
 ReactDOM.createRoot(document.getElementById("root")).render(
